649dea05d35846ea3f8967370d1c0150b98fcbb7
Peggy2344 <peggy102104@gmail.com>
2021-02-02 11:17:01 +0800

    edit

--- src/components/OrderCard.vue
+++ src/components/OrderCard.vue
@@ -112,7 +112,7 @@ export default {
       }
       // const index = this.cart.indexOf(item)
       if (this.user.id) {
-        await delOrder({ userId: this.user.id, orderId: this.orderId, pId: item.productId })
+        await delOrder({ userId: this.user.id, orderId: this.orderId, pId: item._id })
       }
       this.$emit('delCart', item)
       // this.cart.splice(index, 1)
